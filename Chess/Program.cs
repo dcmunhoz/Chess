c9894f77f43affb2c361c618ac0d667bfff3dde1
dcmunhoz <dc.munhoz@hotmail.com>
2021-06-28 21:22:16 -0300

    Criando classe Posição

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace Chess
 {
@@ -6,7 +7,11 @@ namespace Chess
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Posicao p = new Posicao(3,4);
+
+            Console.WriteLine(p);
+
         }
     }
 }
