4046dc50a358d38d1eca9c2c74d9bd6fd956ba52
dcmunhoz <dc.munhoz@hotmail.com>
2021-07-03 16:35:09 -0300

    Jogada especial Promocao

--- Chess/Chess/PatidaDeXadrez.cs
+++ Chess/Chess/PatidaDeXadrez.cs
@@ -318,6 +318,22 @@ namespace Chess
                 throw new BoardException("Você não pode se colocar em xeque !");
             }
 
+            Peca p = Tab.Peca(destino);
+
+            // #jogadaespecial Promocao
+            if ( p is Peao )
+            {
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -335,7 +351,6 @@ namespace Chess
                 MudaJogador();
             }
 
-            Peca p = Tab.Peca(destino);
             
             // #jogadaespecial En Passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
